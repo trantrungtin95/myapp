6719d0ff035101f9fa09c5366a851da7d6739872
tin tran <tintran@Macbooks-MacBook-Pro.local>
2020-08-01 10:41:51 +0700

    Model cart and its line items

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,4 +7,18 @@ class Product < ActiveRecord::Base
         :with => %r{\.(gif|jpg|png|jpeg)\Z}i,
         :message => 'Chi nhan file GIF, JPG, PNG, JPEG'
     }
+
+    has_many :line_items
+    before_destroy :check_if_has_line_item
+  
+     private
+  
+    def check_if_has_line_item
+        if line_items.empty?
+            return true
+        else
+            errors.add(:base, 'This product has a LineItem')
+            return false
+        end
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_01_031404) do
+ActiveRecord::Schema.define(version: 2020_08_01_032410) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -34,6 +34,13 @@ ActiveRecord::Schema.define(version: 2020_08_01_031404) do
   end
 
   create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "line_items", force: :cascade do |t|
+    t.integer "product_id"
+    t.integer "cart_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
