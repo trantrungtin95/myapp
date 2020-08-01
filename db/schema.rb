437aeebd7c8a5fb273a4eb22d4d140ed6d262a77
tin tran <tintran@Macbooks-MacBook-Pro.local>
2020-08-01 10:50:35 +0700

    Add quantity to cart

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_01_032410) do
+ActiveRecord::Schema.define(version: 2020_08_01_034411) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2020_08_01_032410) do
     t.integer "cart_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "quantity"
   end
 
   create_table "products", force: :cascade do |t|
