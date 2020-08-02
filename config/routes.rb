a038caba83a1eb7acb1cbb2b18927d1bad264ab9
tin tran <tintran@Macbooks-MacBook-Pro.local>
2020-08-02 16:41:30 +0700

    FAQ static page

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,19 @@ Rails.application.routes.draw do
   resources :orders
   resources :line_items
   resources :carts
+
+  resources :statics do
+    collection do 
+      get :faq
+    end
+  end
+
   get 'store/index'
   resources :products
   root :to => 'store#index'
   get 'say/hello'
   get 'say/goodbye'
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
