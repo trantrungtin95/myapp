bd1296471ddb19a0927f97399214062cf984a9d1
tin tran <tintran@Macbooks-MacBook-Pro.local>
2020-08-10 20:22:12 +0700

    Disable Image format for development

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,10 +5,10 @@ class Product < ActiveRecord::Base
     validates :title, :description, :image_url, :presence => true
     validates :price, :numericality =>{:greater_than_or_equal_to => 1.0}
     validates :title, :uniqueness => true
-    validates :image_url, :format =>{
-        :with => %r{\.(gif|jpg|png|jpeg)\Z}i,
-        :message => 'Chi nhan file GIF, JPG, PNG, JPEG'
-    }
+    # validates :image_url, :format =>{
+    #     :with => %r{\.(gif|jpg|png|jpeg)\Z}i,
+    #     :message => 'Chi nhan file GIF, JPG, PNG, JPEG'
+    # }
 
     has_many :line_items
     before_destroy :check_if_has_line_item
