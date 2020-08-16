fe1a17d7f77928c8ea4911619a3e0c08802b02d3
tin tran <tintran@Macbooks-MacBook-Pro.local>
2020-08-16 15:03:21 +0700

    Recursive generate comment content

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,2 +1,22 @@
 module CommentsHelper
+    def generate_comment_content(comment)
+        # TODO: using recursive or while do 
+        # generate comment content
+        # html. It's not ruby
+        #<p class='comment' id='comment-<%= comment.id %>' ><strong><%= comment.user.name %></strong> said: <%= comment.content  %>
+#   - <i>created <%= time_ago_in_words(comment.created_at) %> ago</i>
+#   <%= link_to 'Reply', new_product_review_comment_path(@product, comment.review, comment_id: comment.id), remote: true %>
+
+#  </p> 
+        content = content_tag(:p, class: 'comment', id: "comment-#{comment.id}") do
+            tag = content_tag(:strong, comment.user.name + " said : ")
+            tag += comment.content
+            tag
+        end
+        # TODO: add level of comment (children comment for alignment)
+        comment.children_comments.each do |children_comment|
+            content += generate_comment_content(children_comment)
+        end
+        content
+    end
 end
